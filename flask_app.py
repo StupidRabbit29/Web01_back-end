64b1bc421cb427843cd2dd7150b2783d5d063d33
StupidRabbit29 <48109681+StupidRabbit29@users.noreply.github.com>
2020-12-05 16:42:38 +0800

    update signin

--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,53 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, g, request
+from webargs import fields
+from webargs.flaskparser import use_args
 import flask_cors
 import flask_restful as restful
+import sqlite3
+
+DATABASE = 'database/callup_system.db'
 
 app = Flask(__name__)
+api = restful.Api(app)
 # 允许跨域请求，因为后端是运行在5000端口上的，而前端是3000端口
 flask_cors.CORS(app, supports_credentials=True)
 
+@app.before_request
+def before_request():
+  g.db = sqlite3.connect(DATABASE)
 
-@app.route('/login/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    print('User %s' % escape(username))
-    return {
-        'result': 'success'
-    }
+@app.teardown_request
+def teardown_request(exception):
+  if hasattr(g, 'db'):
+    g.db.close()
+
+def query_db(query, args=(), onlyonerow=False):
+    c = g.db.execute(query, args)
+    rv = [dict((c.description[idx][0], value) for idx, value in enumerate(row)) for row in c.fetchall()]
+    return (rv[0] if rv else None) if onlyonerow else rv
 
 
+class CheckUserSignin(restful.Resource):
+    @use_args({
+        'name': fields.Str(required=True),
+        'password': fields.Str(required=True)
+    }, location='query')
+    def get(self, args):
+        userexist = query_db('select count(*) as count from user where name = ?', (args['name'],), onlyonerow=True)['count']
+        if userexist == 1:
+            rightpassword = query_db('select count(*) as count from user where name = ? and password = ?', (args['name'], args['password']), onlyonerow=True)['count']
+            if rightpassword == 1:
+                userinfo = query_db('select name, phone_num, description, level from user where name = ?', (args['name'],))
+                return {'result': 'success', 'userinfo': userinfo}
+            else:
+                return {'result': 'fail', 'errMsg': 'wrong password'}
+        else:
+            return {'result': 'fail', 'errMsg': 'user not exist'}
+
+api.add_resource(CheckUserSignin, '/signin')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+
 
 
---
+++ init_database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+DATABASE = 'database/callup_system.db'
+
+def create_table(table_name):
+    db = sqlite3.connect(DATABASE)
+    c = db.cursor()
+    c.execute(table_name)
+    db.commit()
+    db.close()
+
+def insert_into_table(table_name, values):
+    db = sqlite3.connect(DATABASE)
+    c = db.cursor()
+    c.execute('insert into ' + table_name + ' values ' + values)
+    db.commit()
+    db.close()
+
+user_table = '''create table user(
+    id int primary key not null,
+    name text not null,
+    password text not null,
+    phone_num text not null,
+    description char(100),
+    user_type int not null, -- 1：普通用户 2：管理员
+    identity_type int not null, -- 1：身份证 2：护照
+    identity_num text not null,
+    level int, -- 1-3等级逐渐增大
+    city text not null,
+    signup_time text not null,
+    modify_time text not null
+);'''
+
+user_HHX = '(1, "HHX", "hhx100", "12345678910", "", 2, 1, 111111111111111111, 3, "beijing", "2020-12-5", "2020-12-5")'
+user_FB = '(2, "FB", "fb100", "12345678911", "", 2, 1, 111111111111111112, 3, "beijing", "2020-12-5", "2020-12-5")'
+
+
+if __name__ == '__main__':
+    insert_into_table("user", user_HHX)
+    insert_into_table("user", user_FB)
